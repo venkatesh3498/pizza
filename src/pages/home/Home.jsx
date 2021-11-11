6bcc64abbcc5db1bf2651d0bde5dbb433533aeeb
venkatesh3498 <venkatesh9553361@gmail.com>
2021-11-11 17:47:00 +0530

    did multiple changes like capsule and deleted data file bcoz no use

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import '../index.css';
 import './home.css';
-
-import React from 'react'
+import React,{useState} from 'react'
 import { Sidebar } from '../../components/sidebar/Sidebar';
 
 function Home() {
+    const [active,setActive] = useState("null");
     return (
         <div className="main">
             <div className="sidebar">
@@ -15,6 +15,44 @@ function Home() {
                     <div className="header">
                         <h2>Welcome To Eatland 🤤</h2>
                         <input type="search" placeholder="search....."/>
+                    </div>
+                    <div className="capsules">
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/000000/external-dish-hotel-services-flatart-icons-lineal-color-flatarticons.png"
+                                className="cap-img"
+                            />
+                            <p>All</p>
+                        </div>
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src='../../Assets/Burger_perspective_matte_s.png'
+                                className="cap-img"
+                            />
+                            <p>Burgers</p>
+                        </div>
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src='./Assets/Pizza_perspective_matte_s.png'
+                                className="cap-img"
+                            />
+                            <p>Pizzas</p>
+                        </div>
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-salad-food-levitation-icongeek26-linear-colour-icongeek26.png"
+                                className="cap-img"
+                            />
+                            <p>Salads</p>
+                        </div>
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src='./Assets/Donut_perspective_matte_s.png' 
+                                className="cap-img"
+                            />
+                            <p>Deserts</p>
+                        </div>
+                        <div className={`capsule ${active}`}  onClick={()=>{setActive("active")}}>
+                            <img src='./Assets/can-dynamic-premium.png'
+                                className="cap-img"
+                            />
+                            <p>Drinks</p>
+                        </div>
                     </div>
                 </div>
                 <div className="cart">
